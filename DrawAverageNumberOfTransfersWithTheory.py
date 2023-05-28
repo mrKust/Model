68e40a81d73842958ace5685e3ee8dd99f7eb469
mrKust <dimvas2010@yandex.ru>
2023-05-28 21:13:10 +0300

    add script for compare average number of transfer

---
+++ DrawAverageNumberOfTransfersWithTheory.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+def lineplotMD(x_data, y_data, x_label="", y_label="", title=""):
+    _, ax = plt.subplots()
+    y2_data = []
+    a = 0.8
+    for x in x_data:
+        if (x == 0):
+            y2_data.append(1 - 0.5*a)
+        else:
+            y2_data.append(pow(0.5*a, x)*(1 - 0.5*a))
+
+    print(y2_data)
+    ax.plot(x_data, y2_data, 'g', lw=4, label=('Теоретические значения'))
+    ax.plot(x_data, y_data, 'b--', lw=4, label=('Практические значения'))
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+
+def main():
+    data = []
+    with open("D:\Pereezd\Labs\Научка\Model\AverageNumberOfWorkTransfersWithTheory.txt") as f:
+        for line in f:
+            data.append([float(x) for x in line.split()])
+
+    print(data)
+    dataX = []
+    dataY = []
+
+    for line in data:
+        dataX.append(line.pop(0))
+        dataY.append(line.pop(0))
+
+    print(dataX)
+    print(dataY)
+
+    lineplotMD(dataX, dataY, "Кол-во передач", "Среднее кол-во переходов задач", "Среднее кол-во переходов задач")
+    plt.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
