425c32b5452a1e4ebec696f24b17482434b924b6
mrKust <dimvas2010@yandex.ru>
2023-06-06 01:00:53 +0300

    up

--- DrawSummaryD.py
+++ DrawSummaryD.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 def lineplotMD(x_data, y_data, y2_data, y3_data, x_label="", y_label="", title=""):
     _, ax = plt.subplots()
-    ax.plot(x_data, y2_data, 'g', lw=4, label=('С доп временем'))
-    ax.plot(x_data, y_data, 'b--', lw=4, label=('Без доп времени'))
-    ax.plot(x_data, y3_data, 'r:', lw=4, label=('Без доп времени + время на трансферы'))
+    ax.plot(x_data, y2_data, '-go', lw=3, label=('С доп временем'))
+    ax.plot(x_data, y_data, '--bo', lw=3, label=('Без доп времени'))
+    ax.plot(x_data, y3_data, ':ro', lw=3, label=('Без доп времени + время на трансферы'))
     ax.set_title(title)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
@@ -12,7 +12,7 @@ def lineplotMD(x_data, y_data, y2_data, y3_data, x_label="", y_label="", title="
 
 def main():
     data = []
-    with open("/Users/da.vasilyev/Desktop/Projects/Model/AverageNumberWorkTransfers.txt") as f:
+    with open("D:\Pereezd\Labs\Научка\Model\AverageNumberWorkTransfers.txt") as f:
         num = 0
         for line in f:
             if num == 0:
@@ -38,7 +38,7 @@ def main():
     print(dataY2)
     print(dataY3)
 
-    lineplotMD(dataX, dataY1, dataY2, dataY3, chr(955), "D(" + chr(955) + ")", "D(" + chr(955) + ")")
+    lineplotMD(dataX, dataY1, dataY2, dataY3, "Входная интенсивность, " + chr(955), "Средняя задержка, D, квант)", "Сравнение значение средней задержки для случая с дополнительным\nвременем на перенос задачи и без времени на перенос")
     plt.legend()
     plt.legend()
 
